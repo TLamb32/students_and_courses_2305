8df12bec8bad828160d8d5be55e9898b40927829
Terry Lamb <terrylamb@gmail.com>
2023-05-24 11:17:36 -0400

    add spec tests

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,30 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
+    before(:each) do
+        @student = Student.new({name: "Morgan", age: 21})  
+    end
+
+    describe '#exists' do
+        it 'exists and has readable attributes' do
+            expect(@student).to be_a(Student)
+            expect(@student.name).to eq "Morgan"
+            expect(@student.age).to eq 21
+            expect(@student.scores).to eq ([])
+        end
+    end
+
+    describe '#logs_scores' do
+        it 'can log scores' do
+            @student.log_score(89)
+            @student.log_score(78)
+
+            expect(@student.scores).to eq([89, 78])
+        end
+    end
+
+    describe '#grade' do
+        it 'can average the scores'
+    end
   
 end
