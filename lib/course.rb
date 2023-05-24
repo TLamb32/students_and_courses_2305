3ad0e4c6003a47d624aba84bfd8f6e398d9c298e
Terry Lamb <terrylamb@gmail.com>
2023-05-24 13:20:26 -0400

    Add course spec and file.

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,25 @@
 class Course
+    attr_reader :name,
+                :capacity,
+                :students
+                
 
+    def initialize(name, capacity, full = false)
+        @name = name
+        @capacity = capacity
+        @students = []
+        @full = full
+    end
+
+    def full?
+        @full
+    end
+
+    def enroll(student)
+        @students << student
+    end
+
+    def full?
+        @full == true
+    end
 end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,39 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do 
+    before(:each) do
+        @course = Course.new("Calculus", 2)
+    end
+
+    describe '#exists' do
+        it 'exists and has readable attributes' do
+            expect(@course.name).to eq "Calculus"
+            expect(@course.capacity).to eq 2
+            expect(@course.students).to eq([]) 
+            expect(@course.full?).to eq false
+        end
+    end
+
+    describe '#students' do
+        it 'has students' do
+            @student1 = Student.new({name: "Morgan", age: 21})
+            @student2 = Student.new({name: "Jordan", age: 29})  
+        end
+    end
+
+    describe '#enroll' do
+        it 'can enroll students' do
+            @course.enroll(@student1) 
+            @course.enroll(@student2) 
+
+            expect(@course.students).to eq([@student1, @student2])
+        end
+    end
+
+    describe '#full' do
+        it 'can be full' do
+            expect(@course.full?).to eq true
+        end
+    end
 
 end
